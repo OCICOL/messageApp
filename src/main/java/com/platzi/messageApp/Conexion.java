82f02cb636497c5e1fa4bd9cfab6640891ce95c5
ocolm <ocolmena@gmail.com>
2023-02-16 18:23:56 -0500

    segundo commit

--- src/main/java/com/platzi/messageApp/Conexion.java
+++ src/main/java/com/platzi/messageApp/Conexion.java
@@ -1,2 +1,21 @@
-package com.platzi.messageApp;public class Conexion {
+package com.platzi.messageApp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection getConnection() throws SQLException {
+        Connection conection = null;
+        try {
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/messageApp","root","");
+            if (conection != null){
+                System.out.println("Conexion Exitosa");
+            };
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conection;
+    }
 }
--- src/main/java/com/platzi/messageApp/Start.java
+++ src/main/java/com/platzi/messageApp/Start.java
@@ -1,2 +1,16 @@
-package com.platzi.messageApp;public class Start {
+package com.platzi.messageApp;
+
+import java.sql.Connection;
+
+public class Start {
+    public static void main(String[] args) {
+
+        Conexion conexion = new Conexion();
+
+        try (Connection cnx = conexion.getConnection()) {
+
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
